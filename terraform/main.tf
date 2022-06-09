abe8595a87df466d0cde4f971ebbcaf3f1ffd1d5
Chiel Kunkels <chiel@kunkels.me>
2022-06-10 01:09:22 +0200

    add deploy key as a secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,13 @@ resource "github_repository" "ci" {
   has_projects           = false
   has_wiki               = false
 }
+
+resource "tls_private_key" "ci_deploy_key" {
+  algorithm = "ED25519"
+}
+
+resource "github_actions_secret" "ci_deploy_key" {
+  repository      = github_repository.ci.name
+  secret_name     = "DEPLOY_PRIVATE_KEY"
+  plaintext_value = tls_private_key.ci_deploy_key.private_key_openssh
+}
