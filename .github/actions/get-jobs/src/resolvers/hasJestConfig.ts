4437a6dd20044bece7ea0c20a1365aa6427e3f95
Chiel Kunkels <chiel@kunkels.me>
2022-06-23 01:46:46 +0200

    remove jest-run job

--- .github/actions/get-jobs/src/resolvers/hasJestConfig.ts
+++
@@ -1,6 +0,0 @@
-import { PackageDescription } from '../types';
-import { fileExists } from '../utils';
-
-export default function hasJestConfig(pkg: PackageDescription): boolean {
-	return fileExists(`${pkg.path}/jest.config.js`);
-}
--- .github/actions/get-jobs/src/resolvers/index.ts
+++ .github/actions/get-jobs/src/resolvers/index.ts
@@ -1,6 +1,5 @@
 import { PackageDescription } from '../types';
 import hasDockerfile from './hasDockerfile';
-import hasJestConfig from './hasJestConfig';
 import hasKubernetesManifest from './hasKubernetesManifest';
 
 type Resolver = (pkg: PackageDescription) => boolean;
@@ -8,7 +7,6 @@ type Resolver = (pkg: PackageDescription) => boolean;
 const resolvers: Record<string, Resolver> = {
 	'docker-build': hasDockerfile,
 	'docker-lint': hasDockerfile,
-	'jest-run': hasJestConfig,
 	'kubernetes-deploy': hasKubernetesManifest,
 };
 
